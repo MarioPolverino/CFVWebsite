6803a44e13025428316bb22dfaf953d3263f81e6
puneet93 <puneet.softweb@gmail.com>
2021-09-21 06:10:14 +0530

    changes

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -1,57 +1,65 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 import {Form, Button, FloatingLabel} from 'react-bootstrap'
 
 function ContactForm() {
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [message, setMessage] = useState('');
-    const [subject, setSubject] = useState('');
+    // const [name, setName] = useState('');
+    // const [email, setEmail] = useState('');
+    // const [message, setMessage] = useState('');
+    // const [subject, setSubject] = useState('');
 
-    const handleSubmit = e => {
-        e.preventDefault();
-        // const data = {
-        //   name,
-        //   email,
-        //   message,
-        //   subject
-        // };
-        // console.log(data);
+    // const handleSubmit = e => {
+    //     e.preventDefault();
+    //     // const data = {
+    //     //   name,
+    //     //   email,
+    //     //   message,
+    //     //   subject
+    //     // };
+    //     // console.log(data);
 
-        // fetch('/api/contact', {
-        //     method: 'post',
-        //     body: JSON.stringify(data),
-        // });
+    //     // fetch('/api/contact', {
+    //     //     method: 'post',
+    //     //     body: JSON.stringify(data),
+    //     // });
 
-        const mailto =
-          `mailto:puneet.softweb@gmail.com?subject=${subject} | ${name}&body=Hello CFV, %0D%0A %0D%0A Please contact me: ${email} %0D%0A %0D%0A ${message} %0D%0A %0D%0A Regards, %0D%0A ${name} %0D%0A %0D%0A`;
-        window.location.href = mailto;
-    };
-
+    //     const mailto =
+    //       `mailto:puneet.softweb@gmail.com?subject=${subject} | ${name}&body=Hello CFV, %0D%0A %0D%0A Please contact me: ${email} %0D%0A %0D%0A ${message} %0D%0A %0D%0A Regards, %0D%0A ${name} %0D%0A %0D%0A`;
+    //     window.location.href = mailto;
+    // };
+// onSubmit={handleSubmit}
     return (
-        <Form onSubmit={handleSubmit}>
+        <Form name="contact" method="POST" data-netlify="true">
+            <input type="hidden" name="form-name" value="contact" />
             <FloatingLabel
                 controlId="floatingInput"
                 label="Name"
                 className="mb-2"
-            ><Form.Control type="text" onChange={e => setName(e.target.value)} placeholder="Name" />
+            >
+                {/* <Form.Control type="text" onChange={e => setName(e.target.value)} placeholder="Name" /> */}
+                <Form.Control type="text" name="name" placeholder="Name" />
             </FloatingLabel>
             <FloatingLabel
                 controlId="floatingInput"
                 label="Email *"
                 className="mb-2"
-            ><Form.Control type="text" onChange={e => setEmail(e.target.value)} placeholder="Email *" required={true} />
+            >
+                {/* <Form.Control type="text" onChange={e => setEmail(e.target.value)} placeholder="Email *" required={true} /> */}
+                <Form.Control type="text" name="email" placeholder="Email *" required={true} />
             </FloatingLabel>
             <FloatingLabel
                 controlId="floatingInput"
                 label="Subject"
                 className="mb-2"
-            ><Form.Control type="text" onChange={e => setSubject(e.target.value)} placeholder="Subject" />
+            >
+                {/* <Form.Control type="text" onChange={e => setSubject(e.target.value)} placeholder="Subject" /> */}
+                <Form.Control type="text" name="subject" placeholder="Subject" />
             </FloatingLabel>
             <FloatingLabel controlId="floatingTextarea2" label="Message">
                 <Form.Control
                 as="textarea"
                 className="mb-4"
-                onChange={e => setMessage(e.target.value)}
+                // onChange={e => setMessage(e.target.value)}
+                name="message"
                 placeholder="Message"
                 style={{ height: '100px' }}
                 />
